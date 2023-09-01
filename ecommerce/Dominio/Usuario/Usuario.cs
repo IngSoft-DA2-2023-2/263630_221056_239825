27d7a3144b72940505c9bca2f51fe5492c167799
martinedelman <martin@edelman.com.uy>
2023-09-01 16:45:54 -0300

    ADD - Usuario asbtracto y sus hijos

--- ecommerce/Dominio/Usuario/Usuario.cs
+++ ecommerce/Dominio/Usuario/Usuario.cs
@@ -1,19 +1,15 @@
-﻿using Dominio.Enum;
-
-namespace Dominio
+﻿namespace Dominio.Usuario
 {
-    public class Usuario
+    public abstract class Usuario
     {
         public string CorreoElectronico { get; set; }
-        public Roles Rol { get; set; }
         public string DireccionEntrega { get; set; }
         public List<Compra> Compras { get; set; }
         public bool EstaLogueado { get; set; }
 
-        public Usuario(string correoElectronico, Roles rol, string direccionEntrega)
+        public Usuario(string correoElectronico, string direccionEntrega)
         {
             CorreoElectronico = correoElectronico;
-            Rol = rol;
             DireccionEntrega = direccionEntrega;
             Compras = new List<Compra>();
             EstaLogueado = false;
