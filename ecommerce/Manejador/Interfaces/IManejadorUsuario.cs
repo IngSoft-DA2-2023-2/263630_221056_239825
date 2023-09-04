5d2de9e5f4e8dd2c8a96e647efcb6484c7f056dd
martinedelman <martin@edelman.com.uy>
2023-09-04 10:21:12 -0300

    ADD - Interfaz del usuario con sus metodos

--- ecommerce/Manejador/Interfaces/IManejadorUsuario.cs
+++ ecommerce/Manejador/Interfaces/IManejadorUsuario.cs
@@ -1,7 +1,15 @@
-﻿namespace Manejador.Interfaces
+﻿using Dominio;
+using Dominio.Usuario;
+
+namespace Manejador.Interfaces
 {
     public interface IManejadorUsuario
     {
-
+        Usuario ObtenerUsuario(int id);
+        void RegistrarUsuario(string correoElectronico, string direccionEntrega);
+        List<Usuario> ObtenerUsuarios();
+        void ActualizarUsuario(int id, string direccionEntrega);
+        List<Compra> ObtenerComprasDelUsuario(int id);
+        void AgregarCompraAlUsuario(int id, Compra compra);
     }
 }
