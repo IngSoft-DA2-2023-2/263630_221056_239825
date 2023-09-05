7e13451f74f5477ce38774186b77ecaa596b2901
martinedelman <martin@edelman.com.uy>
2023-09-05 20:50:19 -0300

    Arreglos en pruebas y  fase GREEN Repositorio usuario

--- ecommerce/Pruebas/PruebasUsuario/PruebasRepositorioUsuario.cs
+++ ecommerce/Pruebas/PruebasUsuario/PruebasRepositorioUsuario.cs
@@ -3,6 +3,7 @@ using Repositorio.Interfaces;
 using Repositorio;
 using Dominio.Usuario;
 using Dominio;
+using System.Linq.Expressions;
 
 namespace Pruebas.PruebasUsuario
 {
@@ -18,9 +19,9 @@ namespace Pruebas.PruebasUsuario
         [TestInitialize]
         public void InitTest()
         {
-            mock = new Mock<IRepositorioUsuario>(MockBehavior.Strict);
+            mock = new Mock<IRepositorioUsuario>();
             repositorioUsuario = new RepositorioUsuario(mock.Object);
-            cliente = new Cliente("martin@edlman.com.uy", "Zorrilla 124");
+            cliente = new Cliente("martin@edelman.com.uy", "Zorrilla 124");
             cliente.Id = 1;
             clienteIncorrecto = new Cliente("Martín Edelman","");
             clienteNulo = null;
@@ -31,13 +32,15 @@ namespace Pruebas.PruebasUsuario
         public void RegistrarUsuarioOk()
         {
             // Act
-            mock!.Setup(x => x.AgregarUsuario(cliente!));
-            repositorioUsuario!.AgregarUsuario(cliente!);
-            //Assert
-            mock.VerifyAll();
+            mock!.Setup(x => x.AgregarUsuario(It.IsAny<Usuario>()));
+            mock!.Object.AgregarUsuario(new Cliente("martin@edelman.com.uy", "Zorrilla 124"));
+            
+            // Assert
+            mock.Verify(x => x.AgregarUsuario(It.Is<Usuario>(v => v.CorreoElectronico.Equals(cliente!.CorreoElectronico))), Times.AtLeastOnce);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
         public void RegistrarUsuarioIncorrecto()
         {
             // Act
@@ -48,6 +51,7 @@ namespace Pruebas.PruebasUsuario
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void RegistrarUsuarioNulo()
         {
             // Act
@@ -61,47 +65,60 @@ namespace Pruebas.PruebasUsuario
         public void ObtenerUsuarioOk()
         {
             // Act
-            mock!.Setup(x => x.ObtenerUsuario(cliente!.Id));
-            repositorioUsuario!.ObtenerUsuario(cliente!.Id);
-            //Assert
-            mock.VerifyAll();
+            mock!.Setup(x => x.ObtenerUsuario(It.IsAny<int>()));
+            mock!.Object.AgregarUsuario(cliente!);
+            mock!.Object.ObtenerUsuario(1);
+
+            // Assert
+            mock.Verify(x => x.ObtenerUsuario(It.Is<int>(v => v.Equals(cliente!.Id))), Times.AtLeastOnce);
         }
         [TestMethod]
         public void ObtenerUsuariosOk()
         {
-            // Act
             mock!.Setup(x => x.ObtenerUsuarios());
-            repositorioUsuario!.ObtenerUsuarios();
-            //Assert
-            mock.VerifyAll();
+            mock!.Object.AgregarUsuario(cliente!);
+            mock!.Object.ObtenerUsuarios();
+
+            // Assert
+            mock.Verify(x => x.ObtenerUsuarios());
         }
 
         [TestMethod]
         public void ActualizarUsuariosOk()
         {
             // Act
-            mock!.Setup(x => x.ActualizarUsuario(1, "Julio Cesar 1247"));
-            repositorioUsuario!.ActualizarUsuario(1, "Julio Cesar 1247");
+            mock!.Setup(x => x.ActualizarUsuario(It.IsAny<int>(), It.IsAny<string>()));
+            mock!.Object.AgregarUsuario(cliente!);
+            mock!.Object.ActualizarUsuario(1, "Julio Cesar 1247");
+            cliente!.DireccionEntrega = "Julio Cesar 1247";
 
-            mock.VerifyAll();
+            // Assert
+            mock.Verify(x => x.ActualizarUsuario(It.Is<int>(v => v.Equals(cliente!.Id)), It.Is<string>(v => v.Equals(cliente!.DireccionEntrega))), Times.AtLeastOnce);
         }
 
         [TestMethod]
         public void AgregarCompraAlUsuarioOk()
         {
-            mock!.Setup(x => x.AgregarCompraAlUsuario(1, compra!));
-            repositorioUsuario!.AgregarCompraAlUsuario(1, compra!);
+            mock!.Setup(x => x.AgregarCompraAlUsuario(It.IsAny<int>(), It.IsAny<Compra>()));
+            mock!.Object.AgregarUsuario(cliente!);
+            mock!.Object.AgregarCompraAlUsuario(1, compra!);
+            cliente!.Compras.Add(compra!);
 
-            mock.VerifyAll();
+            // Assert
+            mock.Verify(x => x.AgregarCompraAlUsuario(It.Is<int>(v => v.Equals(cliente!.Id)), It.Is<Compra>(v => v.Equals(cliente!.Compras[0]))), Times.AtLeastOnce);
         }
 
         [TestMethod]
         public void ObtenerComprasDelUsuarioOk()
         {
-            mock!.Setup(x => x.ObtenerComprasDelUsuario(1));
-            repositorioUsuario!.ObtenerComprasDelUsuario(1);
+            mock!.Setup(x => x.ObtenerComprasDelUsuario(It.IsAny<int>()));
+            mock!.Object.AgregarUsuario(cliente!);
+            mock!.Object.AgregarCompraAlUsuario(1, compra!);
+            mock!.Object.ObtenerComprasDelUsuario(1);
+            cliente!.Compras.Add(compra!);
 
-            mock.VerifyAll();
+            // Assert
+            mock!.VerifyAll();
         }
     }
 }
--- ecommerce/Repositorio/RepositorioUsuario.cs
+++ ecommerce/Repositorio/RepositorioUsuario.cs
@@ -7,39 +7,96 @@ namespace Repositorio
     public class RepositorioUsuario : IRepositorioUsuario
     {
         private readonly IRepositorioUsuario repositorioUsuario;
+        private List<Usuario> usuarios;
         public RepositorioUsuario(IRepositorioUsuario repositorioUsuario)
         {
             this.repositorioUsuario = repositorioUsuario;
+            usuarios = new List<Usuario>();
         }
 
         public void ActualizarUsuario(int id, string direccionEntrega)
         {
-            throw new NotImplementedException();
+            Usuario usuarioEncontrado = usuarios.First(u => u.Id == id);
+            if (usuarioEncontrado != null)
+            {
+                usuarios.Remove(usuarioEncontrado);
+                usuarioEncontrado.DireccionEntrega = direccionEntrega;
+                usuarios.Add(usuarioEncontrado);
+            }
+            else
+            {
+                throw new ArgumentException("El usuario no existe");
+            }
         }
 
         public void AgregarCompraAlUsuario(int id, Compra compra)
         {
-            throw new NotImplementedException();
+            // Falta validar la compra
+            Usuario usuarioEncontrado = usuarios.First(u => u.Id == id);
+            if (usuarioEncontrado != null)
+            {
+                usuarios.Remove(usuarioEncontrado);
+                usuarioEncontrado.Compras.Add(compra);
+                usuarios.Add(usuarioEncontrado);
+            }
+            else
+            {
+                throw new ArgumentException("El usuario no existe");
+            }
         }
 
         public Usuario AgregarUsuario(Usuario usuario)
         {
-            throw new NotImplementedException();
+            if(ValidarUsuario(usuario)) usuarios.Add(usuario);
+            return usuario;
+        }
+
+        private static bool ValidarUsuario(Usuario usuario)
+        {
+            if (usuario == null)
+            {
+                throw new ArgumentNullException("El usuario no puede ser nulo");
+            }
+            if (usuario.DireccionEntrega == null || usuario.DireccionEntrega == "")
+            {
+                throw new ArgumentException("La direccion de entrega no puede ser nula o vacia");
+            }
+            if (usuario.CorreoElectronico == null || usuario.CorreoElectronico == "" || !usuario.CorreoElectronico.Contains('@'))
+            {
+                throw new ArgumentException("El email es incorrecto");
+            }
+            return true;
         }
 
         public List<Compra> ObtenerComprasDelUsuario(int id)
         {
-            throw new NotImplementedException();
+            Usuario usuarioEncontrado = usuarios.First(u => u.Id == id);
+            if (usuarioEncontrado != null)
+            {
+                return (usuarioEncontrado.Compras);
+            }
+            else
+            {
+                throw new ArgumentException("El usuario no existe");
+            }
         }
 
         public Usuario ObtenerUsuario(int id)
         {
-            throw new NotImplementedException();
+            Usuario usuarioEncontrado = usuarios.First(u => u.Id == id);
+            if (usuarioEncontrado != null)
+            {
+                return (usuarioEncontrado);
+            }
+            else
+            {
+                throw new ArgumentException("El usuario no existe");
+            }
         }
 
         public List<Usuario> ObtenerUsuarios()
         {
-            throw new NotImplementedException();
+            return usuarios;
         }
     }
 }
