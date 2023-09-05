562d974034ce124c1f36b75200669974238fe97b
Tomás Bañales <tb239825@fi365.ort.edu.uy>
2023-09-04 21:24:02 -0300

    Boceto de Producto previo a implementacion de EF y funciones

--- ecommerce/Dominio/Producto.cs
+++ ecommerce/Dominio/Producto.cs
@@ -2,6 +2,21 @@
 {
     public class Producto
     {
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public string Color { get; set; }
 
+        public Producto(string nombre, int precio, string descripcion, string marca, string categoria, string color)
+        {
+            Nombre = nombre;
+            Precio = precio;
+            Descripcion = descripcion;
+            Marca = marca;
+            Categoria = categoria;
+            Color = color;
+        }
     }
 }
