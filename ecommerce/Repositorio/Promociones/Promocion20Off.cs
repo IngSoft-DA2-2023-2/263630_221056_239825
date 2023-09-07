4dff359047929233ab8c171cd7215f37a831614d
tati pozna <tatipozna@MacBook-Air-de-tati.local>
2023-09-07 18:56:53 -0300

    Promociones 20 off

--- ecommerce/Repositorio/Promociones/Promocion20Off.cs
+++ ecommerce/Repositorio/Promociones/Promocion20Off.cs
@@ -11,17 +11,47 @@ namespace Repositorio.Promociones
 
         public int AplicarPromocion(List<Producto> listaCompra)
         {
-            throw new NotImplementedException();
+            if(listaCompra.Count < 2)
+            {
+                throw new InvalidOperationException("La promocion se aplica si hay, al menos, 2 productos en el carrito");
+            }
+            else
+            {
+                int costoTotal = 0;
+                Producto productoMayorValor = listaCompra.OrderByDescending(p => p.Precio).First();
+                int productoConDescuento = (int)(productoMayorValor.Precio * 0.80);
+                bool esPrimero = true;
+                foreach(Producto p in listaCompra)
+                {
+                    if (esPrimero)
+                    {
+                        p.Precio = productoConDescuento;
+                        esPrimero = false;
+                    }
+                    costoTotal += p.Precio;
+
+                }
+
+                return costoTotal;
+            }           
         }
 
         public string NombrePromocion()
         {
-            throw new NotImplementedException();
+            return "Se aplico un 20% de descuento en el producto de mayor valor";
         }
 
         public bool AplicarPromo(List<Producto> carrito)
         {
-            throw new NotImplementedException();
+            if (carrito.Count < 2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            //Verificar que no sean nulos
         }
 
     }
