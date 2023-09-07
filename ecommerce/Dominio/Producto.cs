9bebcf446507f13e74d86ff75fc472e1ee48384b
Tomás Bañales <tb239825@fi365.ort.edu.uy>
2023-09-07 18:12:59 -0300

    Establecimiento de clases relacionadas a los productos

--- ecommerce/Dominio/Producto.cs
+++ ecommerce/Dominio/Producto.cs
@@ -5,11 +5,11 @@ namespace Dominio
         public string Nombre { get; set; }
         public int Precio { get; set; }
         public string Descripcion { get; set; }
-        public List<string> Marcas { get; set; }
-        public List<string> Categorias { get; set; }
-        public List<string> Colores { get; set; }
+        public Marca Marcas { get; set; }
+        public Categoria Categorias { get; set; }
+        public Color Colores { get; set; }
 
-        public Producto(string nombre, int precio, string descripcion, List<string> marcas, List<string> categorias, List<string> colores)
+        public Producto(string nombre, int precio, string descripcion, Marca marcas, Categoria categorias, Color colores)
         {
             Nombre = nombre;
             Precio = precio;
