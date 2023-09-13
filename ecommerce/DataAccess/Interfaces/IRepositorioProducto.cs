f22cf6b55d8430a766bbafe4ce4209b7bfb62ca9
Tomás Bañales <tb239825@fi365.ort.edu.uy>
2023-09-13 19:24:49 -0300

    Pruebas y logica ServicioProducto y RepositorioProducto por extension

---
+++ ecommerce/DataAccess/Interfaces/IRepositorioProducto.cs
@@ -0,0 +1,10 @@
+﻿using Dominio;
+
+namespace DataAccess;
+
+public interface IRepositorioProducto
+{
+    public void AgregarProducto(Producto productoAgregado);
+    public void EliminarProducto(Producto productoAgregado);
+    public void ModificarProducto(Producto productoNuevo, Producto productoAModificar);
+}
---
+++ ecommerce/DataAccess/RepositorioProducto.cs
@@ -0,0 +1,42 @@
+﻿using Dominio;
+
+namespace DataAccess;
+
+public class RepositorioProducto : IRepositorioProducto
+{
+    private List<Producto> _listaProductos = new List<Producto>();
+
+    public List<Producto> RetornarLista() => _listaProductos;
+
+    public Producto? EncontrarProductoPorId(int id) => _listaProductos.FirstOrDefault(p => p.Id == id);
+    
+    public void AgregarProducto(Producto productoAgregado)
+    {
+        if (ExisteElProducto(productoAgregado))
+        {
+            throw new ArgumentException("El producto ya existe en el sistema.");
+        }
+        _listaProductos.Add(productoAgregado);
+    }
+    
+    public void EliminarProducto(Producto productoABorrar)
+    {
+        if (!ExisteElProducto(productoABorrar))
+        {
+            throw new ArgumentException("El producto ya existe en el sistema.");
+        }
+        _listaProductos.Remove(productoABorrar);
+    }
+    
+    public void ModificarProducto(Producto productoNuevo, Producto productoAModificar)
+    {
+        if (!ExisteElProducto(productoAModificar))
+        {
+            throw new ArgumentException("El producto especificado no fue encontrado.");
+        }
+        EliminarProducto(productoAModificar);
+        AgregarProducto(productoNuevo);
+    }
+
+    private bool ExisteElProducto(Producto productoAgregado) => _listaProductos.FirstOrDefault(p => p.Id == productoAgregado.Id) is not null;
+}
