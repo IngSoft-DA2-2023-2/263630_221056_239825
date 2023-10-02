b14da2395e87d21b8515fae8ba02955b3691d86d
Tomás Bañales <tb239825@fi365.ort.edu.uy>
2023-10-02 03:04:55 -0300

    Implementacion CRUD productos en API

--- ecommerce/Api/Program.cs
+++ ecommerce/Api/Program.cs
@@ -1,7 +1,11 @@
+using Api;
+using ServicioFactory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllers(options => options.Filters.Add(typeof(FiltroExcepciones)));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger(); 
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
---
+++ ecommerce/Dominio/QueryProducto.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Dominio;
+
+public class QueryProducto
+{
+    public string? Nombre { get; set; }
+    public int? Precio { get; set; }
+    public int? MarcaId { get; set; }
+    public int? CategoriaId { get; set; }
+}
+    
