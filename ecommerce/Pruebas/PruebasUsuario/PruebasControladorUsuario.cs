3897551193b2e038bf05bcf054449a2e4cbeb22b
martinedelman <martin@edelman.com.uy>
2023-10-01 21:06:24 -0300

    Primer prueba API Usuario

--- ecommerce/Pruebas/PruebasUsuario/PruebasControladorUsuario.cs
+++ ecommerce/Pruebas/PruebasUsuario/PruebasControladorUsuario.cs
@@ -10,19 +10,27 @@ namespace Pruebas.PruebasUsuario
     [TestClass]
     public class PruebasControladorUsuario
     {
+        private Mock<IManejadorUsuario> usuarioLogicMock = new Mock<IManejadorUsuario>();
+        private Usuario usuario = new("martin@edelman.com.uy", "Julio Cesar 123", "12345678");
+        private ControladorUsuario? controladorUsuario;
+        private List<Usuario>? usuarios;
+        [TestInitialize]
+        public void InitTest()
+        {
+            usuarios = new List<Usuario>()
+            {
+                usuario
+            };
+            usuarioLogicMock.Setup(logic => logic.RegistrarUsuario(usuario)).Returns(usuario);
+            usuarioLogicMock.Setup(logic => logic.ObtenerUsuario(1)).Returns(usuario);
+            usuarioLogicMock.Setup(logic => logic.ObtenerUsuarios()).Returns(usuarios);
+            controladorUsuario = new ControladorUsuario(usuarioLogicMock.Object);
+        }
+
         [TestMethod]
         public void ObtenerTodosLosUsuariosOK()
         {
             // Arrange
-            Usuario usuario = new("martin@edelman.com.uy", "Julio Cesar 123", "12345678");
-            List<Usuario> usuarios = new List<Usuario>()
-            {
-                usuario
-            };
-            Mock<IManejadorUsuario> usuarioLogicMock = new Mock<IManejadorUsuario>();
-            usuarioLogicMock.Setup(logic => logic.ObtenerUsuarios()).Returns(usuarios);
-
-            ControladorUsuario controladorUsuario = new ControladorUsuario(usuarioLogicMock.Object);
             OkObjectResult expected = new OkObjectResult(new List<UsuarioCrearModelo>()
                 { new UsuarioCrearModelo(){
                     CorreoElectronico = usuario.CorreoElectronico,
@@ -31,7 +39,7 @@ namespace Pruebas.PruebasUsuario
                 }});
             List<UsuarioCrearModelo>? ObjetoEsperado = expected.Value as List<UsuarioCrearModelo>;
             // Act
-            OkObjectResult? resultado = controladorUsuario.BuscarTodos() as OkObjectResult;
+            OkObjectResult? resultado = controladorUsuario!.BuscarTodos() as OkObjectResult;
             List<Usuario>? objetoResultado = resultado!.Value as List<Usuario>;
             string mailEsperado = ObjetoEsperado![0].CorreoElectronico;
             string mailResultado = objetoResultado![0].CorreoElectronico;
