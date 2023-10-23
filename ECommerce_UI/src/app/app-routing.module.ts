58eec89e2c1f7ba97fca0b94b71b859401e39fe1
martinedelman <martin@edelman.com.uy>
2023-10-23 12:53:02 -0300

    Acomodación de carpetas UI

--- ECommerce_UI/src/app/app-routing.module.ts
+++ ECommerce_UI/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './components/signup/signup.component';
 import { ProductosComponent } from './components/productos/productos.component';
 
-import { TarjetaComponent } from './card/tarjeta/tarjeta.component';
+import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'productos', component: ProductosComponent },
