34f3a96575f0845ce1737a450183a654dc9f2db3
martinedelman <martin@edelman.com.uy>
2023-10-23 19:27:53 -0300

    Se arregla el DTO del Usuario #15 y #16

---
+++ ecommerce/Api/Dtos/UsuarioDTO.cs
@@ -0,0 +1,22 @@
+﻿using Dominio;
+using Dominio.Usuario;
+
+namespace Api.Dtos
+{
+    public class UsuarioDTO
+    {
+        public string? CorreoElectronico { get; set; }
+        public string? DireccionEntrega { get; set; }
+        public CategoriaRol Rol { get; set; }
+
+        public UsuarioDTO ToEntity(Usuario u)
+        {
+            return new UsuarioDTO()
+            {
+                CorreoElectronico = u.CorreoElectronico,
+                DireccionEntrega = u.DireccionEntrega,
+                Rol = u.Rol,
+            };
+        }
+    }
+}
