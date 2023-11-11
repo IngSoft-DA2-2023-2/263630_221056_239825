e662d94ea76cd29e2290c9384935d283d57257db
tati pozna <tatipozna@gmail.com>
2023-11-11 18:30:40 -0300

    Funcionalidad boton tarjeta

--- ECommerce_UI/src/app/components/opciones-pago/opciones-pago.component.ts
+++ ECommerce_UI/src/app/components/opciones-pago/opciones-pago.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { TarjetaComponent } from "../tarjeta/tarjeta.component";
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-opciones-pago',
-  templateUrl: './opciones-pago.component.html',
-  styleUrls: ['./opciones-pago.component.css'],
-  standalone: true,
-  imports: [MatButtonModule],
+    selector: 'app-opciones-pago',
+    templateUrl: './opciones-pago.component.html',
+    styleUrls: ['./opciones-pago.component.css'],
+    standalone: true,
+    imports: [MatButtonModule, TarjetaComponent, NgIf]
 })
 export class OpcionesPagoComponent {
+  mostrarTarjeta: boolean = false;
 
+  mostrarComponenteTarjeta() {
+    this.mostrarTarjeta = true;
+  }
 }
