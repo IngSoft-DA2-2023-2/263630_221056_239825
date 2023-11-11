f4523bdb2942f2809a13062b140511f8fc23e748
tati pozna <tatipozna@gmail.com>
2023-11-11 18:38:23 -0300

    Funcionalidad boton debito bancario

--- ECommerce_UI/src/app/components/opciones-pago/opciones-pago.component.ts
+++ ECommerce_UI/src/app/components/opciones-pago/opciones-pago.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { TarjetaComponent } from "../tarjeta/tarjeta.component";
-import { NgIf } from '@angular/common';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
     selector: 'app-opciones-pago',
     templateUrl: './opciones-pago.component.html',
     styleUrls: ['./opciones-pago.component.css'],
     standalone: true,
-    imports: [MatButtonModule, TarjetaComponent, NgIf]
+    imports: [MatButtonModule, TarjetaComponent, NgIf, NgFor]
 })
 export class OpcionesPagoComponent {
   mostrarTarjeta: boolean = false;
+  mostrarCuentaBancaria: boolean = false;
 
   mostrarComponenteTarjeta() {
     this.mostrarTarjeta = true;
+  }
+
+  cuentasBancarias: { banco: string; numero: string }[] = [
+    { banco: 'Santander', numero: '1234-5678-9012-3456' },
+    { banco: 'ITAU', numero: '9876-5432-1098-7654' },
+    { banco: 'Banco BBVA', numero: '1111-2222-3333-4444' },
+  ];
+
+  mostrarInfoCuentaBancaria() {
+    this.mostrarCuentaBancaria = true;
+    this.mostrarTarjeta = false; 
   }
 }
