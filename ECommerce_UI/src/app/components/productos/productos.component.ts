8295798a9f69d0e0a82b7f8a519cc8a234cd2496
tati pozna <tatipozna@gmail.com>
2023-11-13 20:22:40 -0300

    Selector promo funciona

--- ECommerce_UI/src/app/components/productos/productos.component.ts
+++ ECommerce_UI/src/app/components/productos/productos.component.ts
@@ -1,47 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductoComponent } from '../producto/producto.component';
-import { NgForOf, NgFor} from '@angular/common';
+import { NgForOf, NgFor, NgIf } from '@angular/common';
 import { Producto } from 'src/app/dominio/producto.model';
 import { ProductsService } from 'src/app/services/productos.services';
 import { catchError, of, take } from 'rxjs';
 import { HttpParams } from '@angular/common/http';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+interface AplicaPromo{
+  nombre: string;
+  valor: boolean;
+}
+
 
 @Component({
   standalone: true,
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css'],
-  imports: [NgForOf, ProductoComponent, NgFor, MatInputModule, MatButtonModule, ReactiveFormsModule]
+  imports: [NgForOf, ProductoComponent, NgFor, MatInputModule, MatButtonModule, ReactiveFormsModule, MatSelectModule, MatFormFieldModule, NgIf]
 })
-export class ProductosComponent implements OnInit{
+export class ProductosComponent implements OnInit {
   filterForm: FormGroup;
   ArrayProductos: Producto[] = [];
 
-  // precioPorOrden: 'mayor' | 'menor' | undefined;
+  precioRango: 'mayor' | 'menor' | undefined;
   porPrecio: number | undefined;
   porCategoria: string | undefined;
   porMarca: string | undefined;
   porNombre: string | undefined;
   filtroActivo: string | undefined;
+  
+  promocion = new FormControl('');
+  aplicaPromo: AplicaPromo[] = [{nombre: 'Aplica promocion', valor: true}, {nombre: 'No aplica promocion', valor: false}];
 
-toggleFiltro(filtro: string) {
-  this.filtroActivo = this.filtroActivo === filtro ? undefined : filtro;
-  this.aplicarFiltros();
-}
+  toggleFiltro(filtro: string) {
+    this.filtroActivo = this.filtroActivo === filtro ? undefined : filtro;
+    this.aplicarFiltros();
+  }
 
-  constructor(private productsServices : ProductsService){
+  constructor(private productsServices: ProductsService) {
     this.filterForm = new FormBuilder().group({
-      porNombre: [''], 
-      // porPrecio: [''],
+      porNombre: [''],
+      porPrecio: [''],
+      precioRango: [''],
       // porCategoria: [''],
       // porMarca: ['']
     });
-   }
-  
-  ngOnInit(){
+  }
+
+  ngOnInit() {
     this.ArrayProductos = this.productsServices.getProducts();
     this.actualizarProductos();
     // this.ArrayProductos.push(this.producto1, this.producto2);
@@ -54,18 +66,23 @@ toggleFiltro(filtro: string) {
   private actualizarProductos() {
     const nombre = this.filterForm.get('porNombre')?.value?.toLowerCase();
     const precio = this.filterForm.get('porPrecio')?.value;
-    const categoria = this.filterForm.get('porCategoria')?.value;
-    const marca = this.filterForm.get('porMarca')?.value;
+    const precioRange = this.filterForm.get('precioRango')?.value;
+    // const categoria = this.filterForm.get('porCategoria')?.value;
+    // const marca = this.filterForm.get('porMarca')?.value;
+    const promo = this.promocion.value;
+   
+    console.log(promo);
 
     const params = new HttpParams()
-      // .set('Precio', precio || '')
+      .set('PrecioEspecifico', precio || '')
       // .set('Categoria', categoria || '')
       // .set('MarcaId', marca || '')
-      .set('Nombre', nombre || '');
+      .set('RangoPrecio', precioRange || '')
+      .set('Nombre', nombre || '')
+      .set('TienePromocion', promo || '');
     this.productsServices.getProductosPorFiltro(params).subscribe((productos) => {
       this.ArrayProductos = productos;
     });
-    // console.log(this.ArrayProductos);
   }
 
   // private producto1: Producto = {
