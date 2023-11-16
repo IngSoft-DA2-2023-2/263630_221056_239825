24408532ae5563dc9b64f3aca6d2c8c6e580c928
martinedelman <martin@edelman.com.uy>
2023-11-16 19:44:42 -0300

    ADD: Guards

--- ECommerce_UI/src/app/app-routing.module.ts
+++ ECommerce_UI/src/app/app-routing.module.ts
@@ -12,19 +12,20 @@ import { OpcionesPagoComponent } from './components/opciones-pago/opciones-pago.
 import { ModificarUsuarioComponent } from './components/modificar-usuario/modificar-usuario.component';
 import { ModificarProductoComponent } from './components/modificar-producto/modificar-producto.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { AuthGuard } from './auth.guard';
 const routes: Routes = [
   { path: '', component: HomepageComponent},
   { path: 'login', component: LoginComponent },
   { path: 'productos', component: ProductosComponent },
   { path: 'carrito', component: CarritoComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'perfil', component: PerfilComponent },
-  { path: 'admin', component: AdminComponent },
-  { path: 'admin/signup', component: SignupComponent },
-  { path: 'admin/editar/usuario/:id', component: ModificarUsuarioComponent },
-  { path: 'admin/editar/producto/:id', component: ModificarProductoComponent},
-  { path: 'admin/agregar/producto', component: ModificarProductoComponent},
-  { path: 'perfil/editar', component: ModificarUsuarioComponent },
+  { path: 'perfil', component: PerfilComponent, canActivate: [AuthGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
+  { path: 'admin/signup', component: SignupComponent, canActivate: [AuthGuard] },
+  { path: 'admin/editar/usuario/:id', component: ModificarUsuarioComponent, canActivate: [AuthGuard] },
+  { path: 'admin/editar/producto/:id', component: ModificarProductoComponent, canActivate: [AuthGuard] },
+  { path: 'admin/agregar/producto', component: ModificarProductoComponent, canActivate: [AuthGuard] },
+  { path: 'perfil/editar', component: ModificarUsuarioComponent, canActivate: [AuthGuard] },
   { path: 'opcionesPago', component: OpcionesPagoComponent}
 ]; 
 
